564ba0661de2b3415c79b013833fd5319fd56c2a
Martin <martinpetkovski@email.com>
2015-09-15 22:49:40 +0200

    bugfixes

--- Main.js
+++ Main.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 	
 	var pam = new PamTypewriter('.homeWrapper .itemsWrapper .subtitle');
 	var leslie = new Leslie();
+
+	leslie.calculateHomeWrapperDimensions();
 	
 	$(window).load(function(){
 		$('.blue').fadeIn(1000);
 		$('.other').delay(700).animate({'fontSize':leslie.getTitleFontSize(10)}, 1000).animate({'opacity': 1}, 1000);
 		$('.contact').delay(2700).fadeIn(500);
-
-		leslie.calculateHomeWrapperDimensions();
 
 		pam.startTypewriting();
 		pam.fixFocus();
