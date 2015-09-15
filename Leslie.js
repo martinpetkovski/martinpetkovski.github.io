b0b236fcc81dc55da96399284371fc9d87367d3f
Martin <martinpetkovski@email.com>
2015-09-15 22:34:16 +0200

    fluid design

--- Leslie.js
+++ Leslie.js
@@ -1,12 +1,63 @@
-root = '/martinpetkovski.net';
-
 var Leslie = (function(){
 	function Leslie() {}
 
+	Leslie.prototype.getTitleFontSize = function(additor) {
+		var title = $(".homeWrapper .title").html().replace(/(<([^>]+)>)/ig,"");
+
+		return ($(window).width() / title.length) + additor;
+	}
+
 	Leslie.prototype.calculateHomeWrapperDimensions = function() {
-		var paddingTop = ($(window).height() / 2) - ( parseInt($(".homeWrapper .title").css("fontSize")) / 2);		
+		var fontSize;
+		var halfFontSize;
+
+		if($(window).width() < 1000) {
+			$(".link span").stop().css({"fontSize": 0});
+			fontSize = this.getTitleFontSize(30);
+			halfFontSize = this.getTitleFontSize(30) / 3;
+		}
+		else {
+			$(".link span").stop().css({"fontSize": 16});
+			fontSize = this.getTitleFontSize(10);
+			halfFontSize = this.getTitleFontSize(10) / 3;
+		}
+
+		$(".homeWrapper .title .blue").css({"fontSize": fontSize});
+		$(".homeWrapper .subtitle").css({"fontSize": halfFontSize});
+		
+		var paddingTop = ($(window).height() / 2) - ( parseInt($(".homeWrapper .title").css("fontSize")) / 2) - 80;		
 		$(".homeWrapper").css("paddingTop", paddingTop);	
-		$(".homeWrapper").css('height', $(window).height() - paddingTop);
+		$(".homeWrapper").css("height", $(window).height() - paddingTop);
+	}
+
+	Leslie.prototype.update = function() {
+		var fontSize;
+		var halfFontSize;
+
+		if($(window).width() < 1000) {
+			$(".link span").stop().animate({"fontSize": 0}, 500);
+			fontSize = this.getTitleFontSize(30);
+			halfFontSize = this.getTitleFontSize(30) / 3;
+		}
+		else {
+			$(".link span").stop().animate({"fontSize": 16}, 500);
+			fontSize = this.getTitleFontSize(10);
+			halfFontSize = this.getTitleFontSize(10) / 3;
+		}
+
+		$(".homeWrapper .title .blue, .homeWrapper .title .other").stop().animate({"fontSize": fontSize}, 500);
+		$(".homeWrapper .subtitle").stop().animate({"fontSize": halfFontSize}, 500);
+
+		var paddingTop = ($(window).height() / 2) - ( parseInt($(".homeWrapper .title").css("fontSize")) / 2) - 80;		
+		$(".homeWrapper").css("paddingTop", paddingTop);	
+		$(".homeWrapper").css("height", $(window).height() - paddingTop);
+	}
+
+	Leslie.prototype.resolutionChange = function() {
+		var that = this;
+		$(window).resize(function() {
+			that.update();
+		});
 	}
 	return Leslie;
 })();
--- m.js
+++ m.js
@@ -2,15 +2,17 @@ $(document).ready(function(){
 	
 	var pam = new PamTypewriter('.homeWrapper .itemsWrapper .subtitle');
 	var leslie = new Leslie();
-
-	leslie.calculateHomeWrapperDimensions();
 	
 	$(window).load(function(){
 		$('.blue').fadeIn(1000);
-		$('.other').delay(700).animate({'fontSize':120}, 1000).animate({'opacity': 1}, 1000);
+		$('.other').delay(700).animate({'fontSize':leslie.getTitleFontSize(10)}, 1000).animate({'opacity': 1}, 1000);
 		$('.contact').delay(2700).fadeIn(500);
+
+		leslie.calculateHomeWrapperDimensions();
 
 		pam.startTypewriting();
 		pam.fixFocus();
+
+		leslie.resolutionChange();
 	});
 });
